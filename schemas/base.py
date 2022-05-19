1273f7d179aedc39661e6d93d5eb1701bee619d2
binkuolo <binkuolo@163.com>
2022-05-20 00:11:46 +0800

    角色、权限、用户 验证模型

--- schemas/base.py
+++ schemas/base.py
@@ -5,8 +5,16 @@
 @Des: 基础schemas
 """
 from pydantic import BaseModel, Field
+from typing import List
 
 
 class BaseResp(BaseModel):
     code: int = Field(description="状态码")
     message: str = Field(description="信息")
+    data: List = Field(description="数据")
+
+
+class ResAntTable(BaseModel):
+    success: bool = Field(description="状态码")
+    data: List = Field(description="数据")
+    total: int = Field(description="总条数")
