f959e114df7181595b8041263cace69abd9056bd
binkuolo <binkuolo@163.com>
2022-05-20 00:06:34 +0800

    包文件

---
+++ views/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+"""
+@Time : 2022/5/18 1:47 PM
+@Author: binkuolo
+@Des: 
+"""
---
+++ views/viewpoints/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+"""
+@Time : 2022/5/18 1:56 PM
+@Author: binkuolo
+@Des: 
+"""
